6ea947a0869879f32eef1ace84a4017b4567a608
Duarte Cordeiro <6994@aebatalha.ccems.pt>
2022-06-28 21:00:42 +0100

    PAP_ProdutoFINAL

---
+++ lib/Cliente/Interface_Ementa/Ementa/lib/src/view/widget/empty_cart.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class EmptyCart extends StatelessWidget {
+  const EmptyCart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+        body: Center(
+        child: Text(
+            "O teu pedido está vazio! \n\n Adiciona itens para submeteres o teu pedido!",
+          style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+        textAlign: TextAlign.center,),
+      ),
+      );
+  }
+}
+
+
